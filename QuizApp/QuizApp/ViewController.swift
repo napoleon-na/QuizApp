f7a500bf370242378827062754c8895bfa885319
Naomi Watanabe <napoleon.na1120@gmail.com>
2018-07-30 20:21:37 +0900

    Create a Xcode project

---
+++ QuizApp/QuizApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Naomi Watanabe on 2018/07/30.
+//  Copyright © 2018年 N Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
